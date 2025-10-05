21a68555669b68a0c7362358a2c2980ccb1323b0
Oleksandr Romaniuk <romanuksashko@gmail.com>
2025-10-05 15:49:49 +0300

    Updates

---
+++ parcel-hw.c9da7e26.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("first"),t=document.getElementById("second"),n=document.getElementById("btn"),d=document.getElementById("result");n.addEventListener("click",()=>{d.value=Math.pow(e.value,t.value)});
+//# sourceMappingURL=parcel-hw.c9da7e26.js.map
